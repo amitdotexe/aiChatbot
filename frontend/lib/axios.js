cec36d8c7f7b2725e83ee3cb089b0c0b9de40ede
amit837-design <amitmallick837@gmail.com>
2026-05-23 11:23:05 +0530

    fixing mitchmatch api calling

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://aichatbot-1a16.onrender.com",
+  baseURL: "https://aichatbot-1a16.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
--- frontend/lib/socket.js
+++ frontend/lib/socket.js
@@ -5,7 +5,7 @@ let socket = null;
 export function getSocket() {
   if (!socket) {
     const token = localStorage.getItem("token");
-    socket = io("https://aichatbot-1a16.onrender.com", {
+    socket = io("https://aichatbot-1a16.onrender.com/api", {
       auth: { token },
       withCredentials: true,
       autoConnect: false,
